23d85f062993d889f1e052928f39b0e1b9f43b23
Ming <minhlaminh01@gmail.com>
2025-10-28 23:47:41 +0700

    refactor: get destination in blog and change style format

--- assets/js/blog-post.js
+++ assets/js/blog-post.js
@@ -33,21 +33,14 @@ async function renderBlogDetail(blog) {
   jq(".blog-detail__title").text(blog.title);
   jq(".blog-detail__date").text(new Date(blog.createdOn).toLocaleDateString());
   jq(".blog-detail__time-read").text(blog.timeRead);
+  jq(".blog-detail__destination").text(blog.destination.name);
 
   await Promise.all([
     getAuthor(blog.authorId),
-    // getDestination(blog.destinationId),
     getThumbnail(blog.thumbnailId),
   ]);
 
   renderBlogContent(blog.content);
-}
-
-async function getDestination(id) {
-  const response = await callApi({
-    url: destinationApi + id,
-  });
-  jq(".blog-detail__destination").text(response.result.name);
 }
 
 async function getThumbnail(id) {
